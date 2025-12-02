b0313be994e05b0ccb790384aeadc2f9fa202c57
Shreyansh Gupta <hardikshrey123@gmail.com>
2025-12-03 00:51:12 +0530

    Update Orbit component styling and replace placeholder circles with camera and flash icons

--- src/components/Orbit.js
+++ src/components/Orbit.js
@@ -5,7 +5,8 @@ export default function Orbit() {
     <div className = "wrapper">
       <div className="circle" />
       <div className="orbitingCircle"> 
-        <div className = "child" />
+        <img src="/dslr2.png" alt="DSLR Camera" className="camera-icon" />
+        <img src="/flash.png" alt="Flash" className="child" />
       </div>
         
     </div>
