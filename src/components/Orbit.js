5108ad061a395ac059712ae54d853351ac5424e5
Shreyansh Gupta <hardikshrey123@gmail.com>
2025-12-03 00:14:02 +0530

    Update README and clean up boilerplate code, add Orbit component

---
+++ src/components/Orbit.js
@@ -0,0 +1,13 @@
+import "./Orbit.css";
+
+export default function Orbit() {
+  return (
+    <div className = "wrapper">
+      <div className="circle" />
+      <div className="orbitingCircle"> 
+        <div className = "child" />
+      </div>
+        
+    </div>
+  );
+}
